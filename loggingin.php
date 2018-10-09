ad8ba609fa29d69980a9c84cbf8f5b2ed68d0b64
statusquonjc46 <njc46@njit.edu>
2018-10-09 15:07:31 -0400

    AUTH ACTUALLY WORKS

--- loggingin.php
+++ loggingin.php
@@ -1,43 +1,32 @@
-
 <?php
-require_once('./home/njc46/490project/rabbitmqphp_example/path.inc');
-require_once('./home/njc46/490project/rabbitmqphp_example/get_host_info.inc');
-require_once('./home/njc46/490project/rabbitmqphp_example/rabbitMQLib.inc');
+require_once('/home/njc46/490project/rabbitmqphp_example/path.inc');
+require_once('/home/njc46/490project/rabbitmqphp_example/get_host_info.inc');
+require_once('/home/njc46/490project/rabbitmqphp_example/rabbitMQLib.inc');
 
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-
+if (isset($argv[1]))
+{
+	$msg = $argv[1];
+}
+else
+{
+	$msg = "test";
+}
 
 $request = array();
 $request['type'] = "login";
-$request['username'] = $_POST["username"];
-$request['password'] = $_POST["pasword"];
+$request['username'] = $_GET["username"];
+$request['password'] = $_GET["password"];
 $request['message'] = "HI";
 $response = $client->send_request($request);
 //$response = $client->publish($request);
 
 echo "client received response: ".PHP_EOL;
-print_r($response);
+$payload = json_encode($response);
+echo $payload;
 echo "\n\n";
 
 echo $argv[0]." END".PHP_EOL;
 ?>
 
-<html>
-<head>
-<title>Logging in..</title>
-<body>
-	<?php
-		if ($response == "0")
-		{
-			session_start();
-			echo "<p>login succesful</p>";
-		}
-		else
-		{
-			echo "<p>login not successful</p>";
-			header('refresh: 3; url=../login.html');
-		}
-	?>
-</body>
-</html>
 
