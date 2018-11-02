5cc75b849a7b2dae17266ba06b110da0d6ab76c1
kchen <kc472@njit.edu>
2018-11-02 11:57:10 -0400

    Created logout.php

---
+++ html/logout.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+session_unset();
+session_destroy();
+?>
+
+<!DOCTYPE>
+<script type = "text/javascript">
+	function countDown(secs, elem){
+		var element = document.getElementById(elem);
+		element.innerHTML = "To sign in, Please wait for <span style = 'color:red;'>"+secs+ "</span> Second(s)";
+		if (secs<1){
+			clearTimeout(timer);
+			window.location = 'index2.html';
+		}
+		secs--;
+		var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+	}
+</script>
+
+<div style="margin:0 auto;text-align:center;" class="container" id="Content">
+<h3>You have Successfully logged out!</h3><br/><p id='status' style="font-size:12px;"></p></div>
+<script type="text/javascript">countDown(5,"status");</script>
+</body>
+</html>
