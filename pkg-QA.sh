f0d5349b8fd0167e695463a587a3a68b2eda8323
kp378 <kp378@njit.edu>
2018-11-30 10:21:32 -0500

    QA code from deployment

--- pkg-QA.sh
+++ pkg-QA.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-
-echo "hello world"
+#this pushes packages out
+scp -r/home/test1/test. test1@192.168.2.194:/home/test1/git/490/html/
+#this pulls packages. Period after html pulls all the files within that dir.
+#scp -r test1@192.168.2.2:/home/test1/git/490/html/. /home/test1/test
