729c8993120c449de8d4100eb08923abf741273a
kc472 <sar49@njit.edu>
2018-11-30 09:44:31 -0500

    add on examples

--- pkg-deploy.sh
+++ pkg-deploy.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+#this pushes packages out
 scp -r /home/test1/git/490/html/. test1@192.168.2.194:/home/test1/test
+#this pulls packages. Period after html pulls all the files within that dir.
+#scp -r test1@192.168.2.2:/home/test1/git/490/html/. /home/test1/test
