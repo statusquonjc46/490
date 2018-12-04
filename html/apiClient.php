acc4f127f58dfb79cb4902e083617c1777077054
test1 <sar49@njit.edu>
2018-12-04 08:39:41 -0500

    Added back apiClient.php for simplicity

---
+++ html/apiClient.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+session_regenerate_id();
+$_SESSION['username'];
+
+require_once('/home/test1/git/490/path.inc');
+require_once('/home/test1/git/490/get_host_info.inc');
+require_once('/home/test1/git/490/rabbitMQLib.inc');
+
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+        $msg = $argv[1];
+}
+else
+{
+        $msg = "test";
+}
+
+$request = array();
+$request['type'] = "api";
+$request['username'] = $_SESSION["username"];
+$request['make'] = $_GET["make"];
+$request['model'] = $_GET["model"];
+$request['year'] = $_GET["year"];
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+#$payload = json_encode($response);
+$payload = $response;
+
+echo "\n\n";
+
+$_SESSION['payload'] = $payload;
+
+echo $argv[1]." END".PHP_EOL;
+
+header('Location: /results.php');
+exit();
+?>
