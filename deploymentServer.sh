3b1e572bb946b82f961961c409bb985933fe2df8
kc472 <sar49@njit.edu>
2018-12-04 12:40:40 -0500

    updated

--- deploymentServer.sh
+++ deploymentServer.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #Dev Machine A pull to server
 scp -r test1@192.168.2.2:~/html.tar ~/git/490
-tar -xvf ~/git/490/html.tar
+tar -xvf ~/git/4tar -cvf ~/Desktop/html.tar /home/njc46/490/490/html
 #server to QA Machine A
-scp -r test1@192.168.2.2:~/html.tar ~/git/490
-tar -xvf ~/git/490/html.tar
-
+tar -cvf ~/Desktop/html.tar /home/njc46/490/490/html
+scp -r ~/Desktop/html.tar test1@192.168.2.194:~/
 #QA Machine A to server
+scp -r test1@192.168.2.194:~/html.tar ~/git/490
+tar -xvf ~/git/4tar -cvf ~/Desktop/html.tar /home/njc46/490/490/html
 #server to prod A
 
