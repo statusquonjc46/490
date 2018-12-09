b2fd037a393d1f28a80596177bf5c1c023d44890
test1 <sar49@njit.edu>
2018-12-09 15:36:13 -0500

    cronEmailer will check for recall updates for a user and send an email based on the crontab setting

--- emailer.php
+++ emailer.php
@@ -21,8 +21,26 @@ class emailer
 		$email->Password = 'cVlQr2w90M';
 		$email->SetFrom('qwertypies490@gmail.com');
 		$email->Subject = 'Welcome '. $username;
-		$email->Body = 'You have signed up for email updates for your '.$year.' '.$make.' '.$model;
+		$email->Body = 'You have signed up for email updates for your '.$year.' '.$make.' '.$model.'.';
 		$email->AddAddress($address);
+
+		$email->Send();
+	}
+
+	function recall_email($address,$username,$make,$model,$year,$summary)
+	{
+		$email = new PHPMailer();
+                $email->isSMTP();
+                $email->SMTPAuth = true;
+                $email->Host = 'smtp.gmail.com';
+                $email->Port = '587';
+                $email->isHTML();
+                $email->Username = 'qwertypies490@gmail.com';
+                $email->Password = 'cVlQr2w90M';
+                $email->SetFrom('qwertypies490@gmail.com');
+                $email->Subject = 'New recall on your '.$year.' '.$make.' '.$model;
+                $email->Body = 'Hello '.$username.','."<br>".'A new recall has been found for your '.$year.' '.$make.' '.$model.'. Here is the summary of the recall:'."<br>".$summary;
+                $email->AddAddress($address);
 
 		$email->Send();
 	}
