9b32dfe21634c2efedbec8721851939f404430ca
test1 <sar49@njit.edu>
2018-12-14 19:38:49 -0500

    QA ssh workaround

--- deployment/pull-back-QA.sh
+++ deployment/pull-back-QA.sh
@@ -1,3 +1,5 @@
 #1/bin/bash
 
 sshpass -p '1password1' scp -r  ~/git/backend$1.tar test1@192.168.1.115:~/git/
+
+sshpass -p '1password1' ssh test1@192.168.1.115 "cd /home/test1/git; tar -xvf backend$1.tar;"
