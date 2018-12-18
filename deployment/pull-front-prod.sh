08f989270e5639b96aca507a5eb834e525a0026a
test1 <sar49@njit.edu>
2018-12-18 09:17:52 -0500

    sudo into var

--- deployment/pull-front-prod.sh
+++ deployment/pull-front-prod.sh
@@ -3,3 +3,5 @@
 sshpass -p '1password1' scp -r  ~/git/versions/frontend/html$1.tar test1@192.168.1.115:~/git/490/html
 
 sshpass -p '1password1' ssh test1@192.168.1.115 "cd /home/test1/git/490/; tar -xvf html$1.tar;"
+
+sudo cp /home/test1/git/490/html /var/www/html/
--- deployment/tar-back.sh
+++ deployment/tar-back.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -cvf ~/Desktop/backend$1.tar --directory=/home/test1/git/versions backend
+tar -cvf ~/Desktop/backend$1.tar --directory=/home/test1/git/490 backend
